3e1e666a9f5d24560106921c59bdb8aa93483257
soumyashree22-antino <soumyashree@antino.com>
2026-06-16 15:14:39 +0530

    port change

--- backend/run.py
+++ backend/run.py
@@ -5,13 +5,15 @@ Usage:
     python run.py
 """
 
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=port,
         reload=True,
         reload_dirs=["./"],
     )
